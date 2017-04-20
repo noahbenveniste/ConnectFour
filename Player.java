322469558bb9a3314e12c641290fe1193bbc6b4f
Sean Devers <spdevers@ncsu.edu>
2017-04-20 14:39:47 -0400

    Added methods to Player.java

--- Player.java
+++ Player.java
@@ -1,3 +1,40 @@
 public class Player{
-
+    
+    private char pieceType;
+    
+    private int moveCount;
+    
+    private int maxConnects;
+    
+    private int numberOfWins;
+    
+    
+    public Player (int num, char pieceType) {
+    
+    ]
+    
+    public int getWins() {
+    
+    }
+    
+    public int getMoveCount() {
+    
+    }
+    
+    public int getMaxConnects() {
+    
+    }
+    
+    public void setWins() {
+    
+    }
+    
+    public void MoveCount() {
+    
+    }
+    
+    public void setMaxConnects() {
+    
+    }
+    
 }
