19b1aaa901689487fa1eea60b9c9eb211aa0a7f5
jequinon <jequinon@ncsu.edu>
2017-04-20 14:23:47 -0400

    Submitting empty class files

---
+++ GameBoard.java
@@ -0,0 +1,37 @@
+public class GameBoard{
+
+  private int numberToConnect;
+  
+  private char[][] grid;
+  
+  private int gridSize;
+  
+  
+  public GameBoard(){
+  
+  }
+  
+  public void displayBoard(){
+  
+  }
+  
+  public int getGridSize(){
+  
+  }
+  
+  public int getBoardNumberToConnect(){
+  
+  }
+  
+  public int checkPlayerWin(char pieceType){
+  
+  }
+  
+  
+  public checkTie(){
+  
+  }
+  
+  
+
+}
