24711312bf48d0424c879efbe37744e82c0043ea
jequinon <jequinon@ncsu.edu>
2017-04-21 20:19:42 -0400

    Added Javadoc and filled simple methods

--- ConnectFour.java
+++ ConnectFour.java
@@ -1,31 +1,57 @@
 public class ConnectFour{
 
-   Player player1;
-   Player player2;
-   GameBoard board;
+   static Player player1;
+   static Player player2;
+   static GameBoard board;
+   
    public static void main(String[] args){
+   
+      player1 = new Player(0,'x');
+      player2 = new Player(0,'o');
+      board = new GameBoard(8);
+   
       boolean playagain = true;
-      while(playagain == true){
+      while(playagain == true){ //While the players want to continue playing
       
-         boolean noWinner
+         boolean noWinner = true;
          while(noWinner){
-         
            
             playerMove(player1);
+            
+            if(board.checkPlayerWin(player1.getPieceType()) == 1       //if player 1 won
+               || board.checkPlayerWin(player1.getPieceType()) == 0){  //or if there was a tie
+               noWinner = false;                                  //then there was a winner(or tie)
+            }
          
             if(noWinner){
-            playerMove(player2);      
+            playerMove(player2); 
             }
             
+            if(board.checkPlayerWin(player2.getPieceType()) == 1       //if player 2 won
+               || board.checkPlayerWin(player2.getPieceType()) == 0){  //or if there was a tie
+               noWinner = false;                                  //then there was a winner(or tie)
+            }
+
+            
          }
+         
+         
+         //TO DO
+         //Handle resetting or exiting the game after someone has won
+         
+         
       }
       
       
       
    }
    
-   public void playerMove(Player player){
-   
+   /**
+    * Manages a players move
+    * @param the player whose move is being made
+    */   
+   public static void playerMove(Player player){
+
    }
 
 }
--- Player.java
+++ Player.java
@@ -1,40 +1,84 @@
+/**
+ * This class models a Player of ConnectFour
+ */
 public class Player{
     
-    private char pieceType;
     
-    private int moveCount;
+    private char pieceType; // The character that will be used as the players piece
     
-    private int maxConnects;
+    private int moveCount; // The number of moves that the player has made
     
-    private int numberOfWins;
+    private int maxConnects; // The highest number of connects that the player currently has
     
-    
+    private int numberOfWins; // Number of wins this player has during this run of the program
+     
+    /**
+     * Constructer for player class
+     * @param num I don't understand what this is for currently
+     * @param pieceType The character to be used as the players piece
+     */
     public Player (int num, char pieceType) {
+       this.pieceType = pieceType;
+    }
     
-    ]
-    
+    /**
+     * Getter method for numberOfWins
+     * @return The number of wins
+     */
     public int getWins() {
+       return numberOfWins;
+    } 
     
+    /**
+     * Getter method for pieceType
+     * @return The character used as the players piece
+     */
+    public char getPieceType() {
+       return pieceType;
     }
     
+    /**
+     * Getter method for moveCount
+     * @return The number of moves that the player has made
+     */
     public int getMoveCount() {
-    
+       return moveCount;
     }
     
+    /**
+     * Getter method for maxConnects
+     * @return The highest number of connects that the player currently has
+     */
     public int getMaxConnects() {
-    
+       return maxConnects;
     }
     
-    public void setWins() {
-    
+    /**
+     * Adds one win to the total number of wins that a player has
+     */
+    public void addWin() {
+       numberOfWins ++;
     }
     
-    public void MoveCount() {
-    
+    /**
+     * Adds one to the total number of moves that a player has made
+     */
+    public void addMoveCount() {
+       moveCount ++;
     }
     
-    public void setMaxConnects() {
+    /** 
+     * Resets the total move count to zero. Mainly used when starting a new game
+     */
+    public void resetMoveCount(){
+       moveCount = 0;
+    }
     
+    /**
+     * Sets the max number of connects
+     */
+    public void setMaxConnects(){
+       
     }
     
 }
