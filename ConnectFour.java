76e8d07cafa4fd11af53294f10f3cd39753e6bb5
nnbenven <nnbenven@ncsu.edu>
2017-04-26 15:05:58 -0400

    Added board size input, test file
    Prompts player for board size input and tests that the input is valid. Also created a whitebox test file

--- ConnectFour.java
+++ ConnectFour.java
@@ -11,17 +11,33 @@ import java.util.*;
 
 public class ConnectFour{
 
+   /** Minimum board width/height */
+   static final int MIN_BOARD_SIZE = 4;
+
+   /** Maximum board width/height */
+   static final int MAX_BOARD_SIZE = 20;
+   
    static Player player1;
    static Player player2;
    static GameBoard board;
    
    public static void main(String[] args){
    
+      Scanner console = new Scanner(System.in);
+      System.out.println("Welcome to Connect Four! Please enter a board size below.");
+      System.out.println("The board size must be a positive, even integer between " + 
+                         MIN_BOARD_SIZE + " and " + MAX_BOARD_SIZE + " inclusive.");
+      System.out.println("The board will be square, with dimensions (board size) x (board size).");
+      System.out.println("Each player will take turns placing pieces in columns until one");
+      System.out.println("player connects their pieces horizontally, vertically or diagonally.");
+      System.out.println("The number of pieces to connect is half the board size.");
+      System.out.println();
+      
+      int boardSize = getBoardSize(console);
+      
       player1 = new Player(1,'x');
       player2 = new Player(2,'o');
-      board = new GameBoard(10);
-      
-      Scanner console = new Scanner(System.in);
+      board = new GameBoard(boardSize);
    
       boolean playagain = true;
       while(playagain == true){ //While the players want to continue playing
@@ -51,15 +67,8 @@ public class ConnectFour{
                || board.checkPlayerWin(player2.getPieceType()) == 0){  //or if there was a tie
                noWinner = false;                                       //then there was a winner(or tie)
             }
-
-            
-         }
-         
-         
-      }
-      
-      
-      
+         } 
+      } 
    }
    
    /**
@@ -78,5 +87,42 @@ public class ConnectFour{
       board.updateBoard(input - 1, player.getPieceType());
       
    }
-
+   
+   /**
+    * Method that prompts user to enter a board size.
+    * If user enters anything other than a positive, even integer,
+    * they are reprompted to enter a size.
+    * @param console A scanner for capturing user input
+    * @return boardSize The length and width of the game board
+    */
+   public static int getBoardSize(Scanner console) {
+      boolean sizeIsValid = false;
+      int boardSize = 0;
+      while(!sizeIsValid){
+         System.out.print("Input size of board: ");
+         while(!console.hasNextInt()) {
+            console.next();
+            System.out.println("Invalid input. Board size must be a positive, " +
+                               "even integer between " + MIN_BOARD_SIZE + " and " +
+                               MAX_BOARD_SIZE + " (inclusive).");
+            System.out.println();
+            System.out.print("Input size of board: ");
+         }
+         boardSize = console.nextInt();
+         if (boardSize < MIN_BOARD_SIZE || boardSize > MAX_BOARD_SIZE || boardSize%2 != 0) {
+            System.out.println("Invalid input. Board size must be a positive, " +
+                               "even integer between " + MIN_BOARD_SIZE + " and " +
+                               MAX_BOARD_SIZE + " (inclusive).");
+            System.out.println();
+         } else {
+            sizeIsValid = true;
+         }
+      }
+      
+      System.out.println("Your board will be " + boardSize + " x " + boardSize + " spaces.");
+      System.out.println();
+      System.out.println();
+      
+      return boardSize;
+   }
 }
---
+++ ConnectFourTest.java
@@ -0,0 +1,5 @@
+public class ConnectFourTest {
+   public static void main(String[] args) {
+   
+   }
+}
