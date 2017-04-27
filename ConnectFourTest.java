e233369f822514b7c50db47548e09dbeb576cb43
nnbenven <nnbenven@ncsu.edu>
2017-04-27 00:28:17 -0400

    final version of whitebox test file

--- ConnectFourTest.java
+++ ConnectFourTest.java
@@ -7,6 +7,7 @@ public class ConnectFourTest {
       testUpdateBoard();
       testValidMove();
       testCheckPlayerWin();
+      testCheckMaxConnects();
    }
    
    public static GameBoard setUp(int size) {
@@ -75,10 +76,86 @@ public class ConnectFourTest {
       System.out.println("Expected: 1");
       System.out.println("Actual: " + board.checkPlayerWin('x'));
       System.out.println();
-   
+      
+      System.out.println("Right diagonal win");
+      board = setUp(6);
+      board.updateBoard(0, 'x');
+      board.updateBoard(1, 'o');
+      board.updateBoard(1, 'x');
+      board.updateBoard(2, 'o');
+      board.updateBoard(2, '0');
+      board.updateBoard(2, 'x');
+      board.displayBoard();
+      System.out.println("Expected: 1");
+      System.out.println("Actual: " + board.checkPlayerWin('x'));
+      System.out.println();
+      
+      System.out.println("No win");
+      board = setUp(4);
+      System.out.println("Expected: -1");
+      System.out.println("Actual: " + board.checkPlayerWin('x'));
+      System.out.println();
+      
+      System.out.println("Tie");
+      board = setUp(4);
+      
+      board.updateBoard(0,'x');
+      board.updateBoard(1,'o');
+      board.updateBoard(2,'x');
+      board.updateBoard(3,'o');
+      
+      board.updateBoard(0,'q');
+      board.updateBoard(1,'q');
+      board.updateBoard(2,'q');
+      board.updateBoard(3,'q');
+      
+      
+      board.updateBoard(0,'x');
+      board.updateBoard(1,'o');
+      board.updateBoard(2,'x');
+      board.updateBoard(3,'o');
+      
+      
+      board.updateBoard(0,'q');
+      board.updateBoard(1,'q');
+      board.updateBoard(2,'q');
+      board.updateBoard(3,'q');
+      
+      board.displayBoard();
+      
+      System.out.println("Expected: 0");
+      System.out.println("Actual: " + board.checkPlayerWin('x'));
+      
    }
    
    public static void testCheckMaxConnects() {
-   
+      System.out.println("Testing checkMaxConnects()");
+      System.out.println();
+      GameBoard board = setUp(8);
+      board.updateBoard(0, 'x');
+      board.updateBoard(1, 'o');
+      board.updateBoard(2, 'o');
+      board.updateBoard(3, 'o');
+      board.updateBoard(5, 'o');
+       
+      board.updateBoard(1, 'x');
+      board.updateBoard(2, 'x');
+      board.updateBoard(3, 'o');
+      
+      board.updateBoard(2, 'x');
+      board.updateBoard(3, 'x');
+      
+      board.updateBoard(3, 'x');
+      
+      board.displayBoard();
+      System.out.println();
+      
+      System.out.println("Max connects for o");
+      System.out.println("Expected: 3");
+      System.out.println("Actual: " + board.checkMaxConnects('o'));
+      System.out.println();
+      System.out.println("Max connects for x");
+      System.out.println("Expected: 4");
+      System.out.println("Actual: " + board.checkMaxConnects('x'));
    }
 }
